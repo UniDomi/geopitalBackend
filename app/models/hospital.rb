3ffdbf17d2e17773b576857fa612fb26429c585b
Manuela Eschler <manu.eschler@gmail.com>
2018-05-10 17:03:04 +0200

    Hospital name and year unique validation

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,5 @@
 class Hospital < ApplicationRecord
 	validates(:name, presence: true)
+	validates(:year, presence: true)
+	validates_uniqueness_of :name, scope: :year
 end
--- test/models/hospital_test.rb
+++ test/models/hospital_test.rb
@@ -14,4 +14,23 @@ class HospitalTest < ActiveSupport::TestCase
   	@hospital.name = ""
   	assert_not @hospital.valid?
   end
+
+  test "year should be present" do
+  	@hospital.year = nil;
+  	assert_not @hospital.valid?
+  end
+
+  test "hospital and year should be unique" do
+  	duplicate_hospital = @hospital.dup
+  	@hospital.save
+  	assert_not duplicate_hospital.valid?
+  end
+
+  test "hospital with other year should be valid" do
+  	duplicate_hospital = @hospital.dup
+  	duplicate_hospital.year = @hospital.year + 1;
+  	@hospital.save
+  	assert duplicate_hospital.valid?
+  end
+
 end
