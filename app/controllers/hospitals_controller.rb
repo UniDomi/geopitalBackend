1abe1eeb1cf425c9d9d7918f7094193ca62659ea
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-12 22:28:02 +0200

    Upload for Hospitals without attributes

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -12,13 +12,23 @@ class HospitalsController < ApplicationController
     @data = Spreadsheet.open 'public'+@upload.attachment_url
     @sheet = @data.worksheet @name
     @legend = @sheet.row(0)
-    @hospital = @sheet.row(2)
-    @test = Hash.new
-    i = 0
-    while i < @legend.length
-      @test.store(@legend[i], @hospital[i])
-      i += 1
+    @hosps = Array.new
+    j = 1
+    while j < @sheet.rows.length
+      @hospital = @sheet.row(j)
+      @hospData = Hash.new
+      i = 0
+      while i < @legend.length
+        @hospData.store(@legend[i], @hospital[i])
+        i += 1
+      end
+      if !Hospital.exists?(name: @hospData["Inst"])
+        @hosps << Hospital.create(name:@hospData["Inst"], streetAndNumber:@hospData["Adr"], zipCodeAndCity:@hospData["Ort"])
+        #@hospData.delete("Inst")
+        #@hospData.delete("Adr")
+        #@hospData.delete("Ort")
+      end
+      j += 1
     end
-    @hosp = Hospital.create(name:@test["Inst"], streetAndNumber:@test["Adr"], zipCodeAndCity:@test["Ort"])
   end
 end
