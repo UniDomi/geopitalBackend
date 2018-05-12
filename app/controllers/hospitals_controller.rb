079126d42c6bd1e192a3992b2b78c0b18358add5
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-12 20:23:17 +0200

    Setuo crude hospital saving

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,5 +1,6 @@
 class HospitalsController < ApplicationController
   def index
+    @hospitals = Hospital.all
   end
 
   def new
@@ -11,13 +12,13 @@ class HospitalsController < ApplicationController
     @data = Spreadsheet.open 'public'+@upload.attachment_url
     @sheet = @data.worksheet @name
     @legend = @sheet.row(0)
-    @hospital = @sheet.row(1)
+    @hospital = @sheet.row(2)
     @test = Hash.new
     i = 0
     while i < @legend.length
       @test.store(@legend[i], @hospital[i])
       i += 1
     end
-
+    @hosp = Hospital.create(name:@test["Inst"], streetAndNumber:@test["Adr"], zipCodeAndCity:@test["Ort"])
   end
 end
