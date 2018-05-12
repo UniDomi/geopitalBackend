420373f3ee085e2750c55a1d36cd2f44f0bb88ed
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-12 17:14:51 +0200

    Code cleanup

--- app/controllers/attribute_types_controller.rb
+++ app/controllers/attribute_types_controller.rb
@@ -11,26 +11,18 @@ class AttributeTypesController < ApplicationController
     @name = (params[:sheet])
     @data = Spreadsheet.open 'public'+@upload.attachment_url
     @sheet = @data.worksheet @name
-    @codes = Array.new
-    @names = Array.new
     @types = Array.new
-    if @name = 'Kennzahlen'
+    if @name == ('Kennzahlen')
       @sheet.each do |row|
         if row[0].present?
           if row[1].present?
-            @codes << (row[0]+row[1])
             @code = (row[0]+row[1])
           else
-            @codes << (row[0])
             @code = (row[0])
           end
-        @names << (row[2])
-        @nameDE = (row[2])
-        @nameFR = (row[3])
-        @nameIT = (row[4])
         end
-        if !AttributeType.exists?(code: @code)
-          @types << AttributeType.create(code: @code, nameDE: @nameDE, nameFR: @nameFR, nameIT: @nameIT)
+        if !AttributeType.exists?(code: @code) || @code.equal?('KZ-Code')
+          @types << AttributeType.create(code: @code, nameDE: row[2], nameFR: row[3], nameIT: row[4])
         end
       end
     end
