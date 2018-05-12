1fbc7127a31d3d7c7fcbf0a76b51dcad63a649a7
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-12 11:26:54 +0200

    Created Uploadfunctionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :uploads, only: [:index, :new, :create, :destroy]
+  get 'uploads/index'
+  get 'uploads/new'
+  get 'uploads/create'
+  get 'uploads/destroy'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'application#hello'
+  #root 'application#hello'
+  root 'uploads#index'
 end
