16f12c722ca6b6dde483c7534cb10acab646a4cb
Manuela Eschler <manu.eschler@gmail.com>
2018-05-12 23:24:47 +0200

    Added newest year to api/hospitals

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,8 @@
 class ApiController < ApplicationController
   def hospitals
+    max_year = HospitalAttribute.maximum("year")
     @hospitals = Hospital
-                     .includes(:hospital_attributes, :hospital_locations)
+                     .includes(:hospital_attributes, :hospital_locations).where(:hospital_attributes => {:year => max_year})
                      .as_json(include: [:hospital_attributes, :hospital_locations])
     render :json => @hospitals
   end
