81f05e922d1b00a05f6f9089b30722b13c8ac601
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-14 22:05:33 +0200

    parsing locations from file

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,30 +14,25 @@ class LocationsController < ApplicationController
     @data = Spreadsheet.open 'public'+@upload.attachment_url
     @sheet = @data.worksheet @name
     @legend = @sheet.row(0)
+    @hospitals = Array.new
     @locs = Array.new
+    @errors = Array.new
     j = 1
-    while j <  5#@sheet.rows.length
+    while j < @sheet.rows.length
       @location = @sheet.row(j)
       @data = Hash.new
       i = 0
       while i < @legend.length
         @data.store(@legend[i], @location[i])
         i += 1
-        @locs << @data
       end
-      #if !Hospital.exists?(name: @hospData["Inst"])
-      #  @hosp = Hospital.create(name:@hospData["Inst"], streetAndNumber:@hospData["Adr"], zipCodeAndCity:@hospData["Ort"])
-        #@hospData.delete("Inst")
-        #@hospData.delete("Adr")
-        #@hospData.delete("Ort")
-      #  @hospData.each do |attr|
-      #    @attribute = @hosp.hospital_attributes.create(code:attr[0], value:attr[1], year:2016, attribute_type:"string")
-      #  end
-      #end
+      @hospital = Hospital.where(name: @data["Inst"]).first
+      if @hospital == nil
+        @errors << 'Not found ' + @data["Inst"]
+      else
+        @locs << @hospital.hospital_locations.create(name: @data["Inst"], kanton: @data["KT"], streetAndNumber: @data["Adr_Standort"], zipCodeAndCity: @data["Ort_Standort"], la: @data["la"])
+      end
       j += 1
     end
-    @hospital = Hospital.where(name: 'RehaClinic Baden')
-    @locs << @hospital
-    
   end
 end
