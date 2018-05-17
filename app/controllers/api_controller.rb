1276e05f1cac6956517d1c6962a671b3817d278f
Manuela Eschler <manu.eschler@gmail.com>
2018-05-17 16:42:28 +0200

    Performance optimization

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,16 +2,16 @@ require 'oj'
 
 class ApiController < ApplicationController
   def hospitals
-    max_year = HospitalAttribute.maximum("year")
-    hospitals = Hospital
-                     .includes(:hospital_attributes, :hospital_locations).where(:hospital_attributes => {:year => max_year})
-                    .as_json(include: [:hospital_attributes, :hospital_locations])
-    render :json => Oj.dump(hospitals)
+    #max_year = HospitalAttribute.maximum("year")
+    #hospitals = Hospital.includes(:hospital_attributes, :hospital_locations).where(:hospital_attributes => {:year => max_year}).as_json(:except => [:created_at, :updated_at], :include => [:hospital_attributes => {:except => [:created_at, :updated_at]}, :hospital_locations => {:except => [:created_at, :updated_at]}])
+    #hospitals = Hospital.first(280).attributes
+    @@data = File.read("hospitals.json")
+    render :json => @@data#Oj.dump(hospitals)
   end
 
   def attributeTypes
-    @attribute_types_string = AttributeType.where(:category => "string")
-    @attribute_types_number = AttributeType.where(:category => "number")
-    render :json => {:attribute_types_string => @attribute_types_string, :attribute_types_number => @attribute_types_number}
+    #@attribute_types_string = AttributeType.where(:category => "string")
+    #@attribute_types_number = AttributeType.where(:category => "number")
+    render :json => {:attribute_types_string => AttributeType.where(:category => "string"), :attribute_types_number => AttributeType.where(:category => "number")}
   end
 end
