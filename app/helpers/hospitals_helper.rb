9982154d9ee4c5abec4260471ab2b26de4a12219
Manuela Eschler <manu.eschler@gmail.com>
2018-05-19 17:08:06 +0200

    outsourced geocoding to HospitalHelper

--- app/helpers/hospitals_helper.rb
+++ app/helpers/hospitals_helper.rb
@@ -1,2 +1,21 @@
 module HospitalsHelper
+  def get_coordinates(hospital)
+    if hospital.zipCodeAndCity == nil
+      return 'No address found for: ' + hospital.name
+    end
+    if hospital.streetAndNumber == nil
+      address = hospital.zipCodeAndCity
+    else
+      address = hospital.streetAndNumber + ", " + hospital.zipCodeAndCity
+    end
+    sleep(0.2)
+    begin
+      @coordinates = Geocoder.coordinates(address)
+    rescue => error
+      return 'No coordinates found for: ' + hospital.name + ' ' + address
+    end
+    hospital.latitude = @coordinates[0]
+    hospital.longitude = @coordinates[1]
+    hospital.save
+   end
 end
