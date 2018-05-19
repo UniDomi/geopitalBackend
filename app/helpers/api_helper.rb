09245e395b37e5109b686cad50c3650b8680b3e5
Manuela Eschler <manu.eschler@gmail.com>
2018-05-19 15:22:24 +0200

    Cache hospitals json
    First time after updating data is still slow

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -3,8 +3,18 @@ module ApiHelper
 
   def create_json
     max_year = HospitalAttribute.maximum("year")
-    hospitals = Hospital.includes(:hospital_attributes).where(:hospital_attributes => {:year => max_year}).as_json(:except => [:created_at, :updated_at], :include => [:hospital_attributes => {:except => [:id, :hospital_id, :created_at, :updated_at]}])
-    return Oj.dump(hospitals)
+    hospitals = Hospital.includes(:hospital_attributes).where(:hospital_attributes => {:year => max_year})
+    hash = Rails.cache.fetch(Hospital.cache_key(hospitals)) do
+      hospitals.as_json(:except => [:created_at, :updated_at], :include => [:hospital_attributes => {:except => [:id, :hospital_id, :created_at, :updated_at]}])
+    end
+    return Oj.dump(hash)
   end
 
+  def create_json_myself
+    max_year = HospitalAttribute.maximum("year")
+
+    @hospitals = Hospital.includes(:hospital_attributes).where(:hospital_attributes => {:year => max_year})
+
+    return MultiJson.dump({hospitals: @hospitals.map {|h| h.attributes}})
+  end
 end
