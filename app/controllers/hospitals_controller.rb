bd6d10a586df6158712f3a355d430a1235998f21
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-19 08:45:44 +0200

    added coords functionality

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -35,13 +35,14 @@ class HospitalsController < ApplicationController
       i = 0
       while i < @legend.length
         @hospData.store(@legend[i], @hospital[i])
+        @hospData.delete("Amb")
+        @hospData.delete("Stat")
+        @hospData.delete("Amb, Stat")
         i += 1
       end
       if !Hospital.exists?(name: @hospData["Inst"])
         @hosp = Hospital.create(name:@hospData["Inst"], streetAndNumber:@hospData["Adr"], zipCodeAndCity:@hospData["Ort"])
-        #@hospData.delete("Inst")
-        #@hospData.delete("Adr")
-        #@hospData.delete("Ort")
+
         @hospData.each do |attr|
           @attribute = @hosp.hospital_attributes.create(code:attr[0], value:attr[1], year:2016)
         end
@@ -49,40 +50,45 @@ class HospitalsController < ApplicationController
       j += 1
     end
   end
-
   def coords
-    # DEMOCOORDS AT THE MOMENT!
-     @hospitals = Hospital.where(latitude: nil)
-     @errors = Array.new
-     @hospitals.each do |hospital|
-       if hospital.streetAndNumber == nil || hospital.zipCodeAndCity == nil
-         @errors << 'No address found for: ' + hospital.name
-         next
-       end
-       lat = 46.905053 + rand(-1..1)
-       lon =  7.981008 + rand(-2..2)
-       hospital.latitude = lat
-       hospital.longitude = lon
-       hospital.save
-       hospital.hospital_locations.each do |loc|
-         lat = 46.905053 + rand(-1..1)
-         lon =  7.981008 + rand(-2..2)
-         loc.latitude = lat
-         loc.longitude = lon
-         loc.save
-       end
-       /
-       address = hospital.streetAndNumber + ", " + hospital.zipCodeAndCity
-       sleep(1)
-       @coordinates = Geocoder.coordinates(address)
-       if !@coordinates == nil
-         hospital.latitude = (@coordinates[0])
-         hospital.longitude = (@coordinates[1])
-         hospital.save
-       else
-         @errors << 'No coordinates found for: ' + hospital.name + ' ' + address
-       end
-       /
-     end
-   end
+    @hospitals = Hospital.where(latitude: nil)
+    @errors = Array.new
+    @hospitals.each do |hospital|
+      if hospital.streetAndNumber == nil || hospital.zipCodeAndCity == nil
+        @errors << 'No address found for: ' + hospital.name
+      else
+        address = hospital.streetAndNumber + ", " + hospital.zipCodeAndCity
+        sleep(1)
+        @coordinates = Geocoder.coordinates(address)
+        if @coordinates != nil
+          hospital.latitude = @coordinates[0]
+          hospital.longitude = @coordinates[1]
+          hospital.save
+        else
+          @errors << 'No coordinates found for: ' + hospital.name + ' ' + address
+        end
+        hospital.hospital_locations.each do |loc|
+          if loc.streetAndNumber == nil || loc.zipCodeAndCity == nil
+            @errors << 'No address found for: ' + loc.name
+            next
+          end
+          loc_address = loc.streetAndNumber + ", " + loc.zipCodeAndCity
+          if address == loc_address
+            loc.latitude = hospital.latitude
+            loc.longitude = hospital.longitude
+          else
+            sleep(1)
+            @coordinates = Geocoder.coordinates(loc_address)
+            if @coordinates != nil
+              loc.latitude = @coordinates[0]
+              loc.longitude = @coordinates[1]
+              loc.save
+            else
+              @errors << 'No coordinates found for: ' + hospital.name + ' ' + address
+            end
+          end
+        end
+      end
+    end
+  end
 end
