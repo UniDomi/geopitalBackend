eb0a90459e5cb5dd90c2233228f6ccda931f393f
Manuela Eschler <manu.eschler@gmail.com>
2018-05-20 23:55:31 +0200

    Parser testing

--- test/controllers/parsers_controller_test.rb
+++ test/controllers/parsers_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class ParsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get parsers_index_url
-    assert_response :success
-  end
-
-  test "should get details" do
-    get parsers_details_url, params{}
-    assert_response :success
-  end
 
 end
