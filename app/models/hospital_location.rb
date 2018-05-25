c1fbba7d463e597220834bf46dc96888311cb59f
Manuela Eschler <manu.eschler@gmail.com>
2018-05-25 11:29:25 +0200

    Added uniqueness to locations model
    name, streetAndNumber, zipCodeAndCity should be unique and tested

--- app/models/hospital_location.rb
+++ app/models/hospital_location.rb
@@ -1,5 +1,5 @@
 class HospitalLocation < ApplicationRecord
-  validates(:name, presence: true)
+  validates(:name, presence: true, uniqueness: {scope: [:streetAndNumber, :zipCodeAndCity]})
   validates(:hospital_id, presence: true)
   belongs_to :hospital
 end
